3d69f2048360303fd77000ef07d6c4be3b1f410c
christian.dollosa@gmail.com <christian.dollosa@gmail.com>
2013-03-12 13:29:10 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upcsspsc::Application.config.secret_token = '90376dcf185b76830f8337855ce912ae6d96b818e3e7f06f3e1383b685c0e50be111da29467488b99227015370c40e14b3004837215b240923809cced23e025a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Upcsspsc::Application.config.session_store :cookie_store, key: '_upcsspsc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Upcsspsc::Application.config.session_store :active_record_store
